b7ea9d2d45a92f9527fe0c4ab7ee2efddb476abf
Hypnos <joashmacer@gmail.com>
2023-12-11 23:05:12 +0300

    chnage

--- lib/War.ex
+++ lib/War.ex
@@ -1,62 +1,33 @@
 defmodule War do
-  @moduledoc """
-    Documentation for `War`.
-  """
+  def deal(shuf) do
+  [player1, player2] = deal_cards(shuf)
+  play_rounds(player1, player2)
+  end
 
-  @doc """
-    Function stub for deal/1 is given below. Feel free to add
-    as many additional helper functions as you want.
+  def deal_cards(shuf) do
+  shuf |> Enum.chunk_every(26)
+  end
 
-    The tests for the deal function can be found in test/war_test.exs.
-    You can add your five test cases to this file.
+  def play_rounds(player1, player2) do
+  case {player1, player2} do
+  {[], []} →
+  {:tie, []}
 
-    Run the tester by executing 'mix test' from the war directory
-    (the one containing mix.exs)
-  """
-    @ace_weight 14
+    {[], player2_deck} ->
+      {:player2, player2_deck}
 
-    def deal(shuf) do
-      {p1, p2} =
-        shuf
-        |> Enum.map(&apply_ace_weight/1)
-        |> deal_deck()
+    {player1_deck, []} ->
+      {:player1, player1_deck}
 
-      winner = play_game(p1, p2)
+    {[card1 | hand1_without_card], [card2 | hand2_without_card]} ->
+      case compare_cards(card1, card2) do
+        :win ->
+          play_rounds(hand1_without_card ++ [card1, card2], hand2_without_card)
 
-      Enum.map(winner, &remove_ace_weight/1)
-    end
+        :lose ->
+          play_rounds(hand1_without_card, hand2_without_card ++ [card1, card2])
 
-    def deal_deck(deck) do
-      {Enum.take_every(deck, 2), Enum.drop_every(deck, 2)}
-    end
-
-    defp play_game(p1, p2, tied \\ [])
-
-    defp play_game([], p2, tied), do: p2 ++ tied
-    defp play_game(p1, [], tied), do: p1 ++ tied
-
-    # War, cards tied
-    defp play_game([c | xs], [c | ys], tied) do
-      cards = Enum.sort([c, c] ++ tied, :desc)
-      play_game(xs, ys, cards)
-    end
-
-    # Normal game turn
-    defp play_game([x | xs], [y | ys], tied) do
-      cards = Enum.sort([x, y] ++ tied, :desc)
-
-      if x > y do
-        play_game(xs ++ cards, ys)
-      else
-        play_game(xs, ys ++ cards)
+        :tie ->
+          play_war(hand1_without_card, hand2_without_card, [card1, card2])
       end
-    end
-
-    defp apply_ace_weight(card) do
-      (card == 1 && @ace_weight) || card
-    end
-
-    defp remove_ace_weight(card) do
-      (card == @ace_weight && 1) || card
-    end
   end
